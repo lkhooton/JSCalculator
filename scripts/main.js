c3abe47c5e0811b74a0af759577962ac930fa13a
Lauren Hooton <lkhooton@gmail.com>
2023-01-22 18:55:13 -0500

    js buttons

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('[data-number')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const clearButton = document.querySelector('[data-all-clear]')
+const previousText = document.querySelector('[data-previous]')
+const currentText = document.querySelector('[data-current]')
+

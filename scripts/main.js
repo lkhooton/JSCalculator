cf9d149cf2851e69168301843ac30b972b71f16b
Lauren Hooton <lkhooton@gmail.com>
2023-01-23 09:51:52 -0500

    js work

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,3 @@
-const numberButtons = document.querySelectorAll('[data-number');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const equalsButton = document.querySelector('[data-equals]');
-const deleteButton = document.querySelector('[data-delete]');
-const clearButton = document.querySelector('[data-all-clear]');
-const previousText = document.querySelector('[data-previous]');
-const currentText = document.querySelector('[data-current]');
-
-
 //store the output
 class Calculator {
   constructor(previousText, currentText) {
@@ -14,10 +5,12 @@ class Calculator {
     this.currentText = currentText;
     this.clear();
   }
-}
+};
 
-clear(){
-
+clear() {
+  this.currentOperand = ''
+  this.previousOperand = ''
+  this.operation = undefined
 }
 
 delete() {
@@ -37,5 +30,24 @@ compute(){
 }
 
 updateDisplay(){
-  
+  this.currentText.innerText = this.currentOperand
+
 }
+
+
+const numberButtons = document.querySelectorAll('[data-number');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const clearButton = document.querySelector('[data-all-clear]');
+const previousText = document.querySelector('[data-previous]');
+const currentText = document.querySelector('[data-current]');
+
+const calculator = new Calculator (previousText, currentText);
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})

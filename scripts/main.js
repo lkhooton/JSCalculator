ef4edf8c4af8dd579aaff81c99f317a953d55abd
Lauren Hooton <lkhooton@gmail.com>
2023-01-22 19:00:48 -0500

    outline functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,41 @@
-const numberButtons = document.querySelectorAll('[data-number')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const clearButton = document.querySelector('[data-all-clear]')
-const previousText = document.querySelector('[data-previous]')
-const currentText = document.querySelector('[data-current]')
+const numberButtons = document.querySelectorAll('[data-number');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const clearButton = document.querySelector('[data-all-clear]');
+const previousText = document.querySelector('[data-previous]');
+const currentText = document.querySelector('[data-current]');
 
+
+//store the output
+class Calculator {
+  constructor(previousText, currentText) {
+    this.previousText = previousText;
+    this.currentText = currentText;
+    this.clear();
+  }
+}
+
+clear(){
+
+}
+
+delete() {
+
+}
+
+appendNumber(number){
+
+}
+
+chooseOperation(operation){
+
+}
+
+compute(){
+
+}
+
+updateDisplay(){
+  
+}
